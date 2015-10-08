a7c7dd5732694e12956986ad6cec4ba689ff787e
mac <mac@wificeshi.local>
2015-10-07 23:24:38 -0700

    Initial Commit

---
+++ MyWeibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyWeibo
+//
+//  Created by mac on 15/10/7.
+//  Copyright (c) 2015年 huiwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
